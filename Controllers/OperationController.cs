15110708adeea622e68df741940ce0d494848d2f
Mohammed Kaif Ansari <kaifansari363363@gmail.com>
2025-10-14 19:16:06 +0530

    first commit

---
+++ Controllers/OperationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewDms.Controllers
+{
+    public class OperationController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
